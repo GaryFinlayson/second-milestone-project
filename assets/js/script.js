b53512fd90001022901084dbbfd0322907449fce
Gary Finlayson <gfinlayson1@hotmail.co.uk>
2021-04-19 15:26:32 +0100

    Changed script.js code to generate table with thead

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,40 @@
 
-$.get('assets/data/CountriesOfEurope.csv',function(data) {
-    var html = '<"table">';
-    var rows = data.split("\n");
-    rows.forEach(function getvalues(tablerow) {
-        html += "<tr>";
-        var columns = tablerow.split(",");
-        html += "<td>" + columns[0] + "</td>";
-        html += "<td>" + columns[1] + "</td>";
-        html += "<td>" + columns[2] + "</td>";
-        html += "<td>" + columns[3] + "</td>";
-        html += "<td>" + columns[4] + "</td>";
-        html += "<td>" + columns[5] + "</td>";
-        html += "<td>" + columns[6] + "</td>";
-        html += "</tr>";
-    })
-    html += "</table>";
+$.ajax({
+    url: 'assets/data/CountriesOfEurope.csv',
+    dataType: 'text',
+  }).done(successFunction);
+
+  function successFunction(data) {
+    var allRows = data.split(/\r?\n|\r/);
+    var html = '<table>';
+    for (var row = 0; row < allRows.length; row++) {
+        if (row === 0) {
+          html += '<thead>';
+          html += '<tr>';
+        } else {
+          html += '<tr>';
+        };
+        var rowCells = allRows[row].split(',');
+        for (var cell = 0; cell < rowCells.length; cell++) {
+          if (row === 0) {
+            html += '<th>';
+            html += rowCells[cell];
+            html += '</th>';
+          } else {
+            html += '<td>';
+            html += rowCells[cell];
+            html += '</td>';
+          };
+        };
+        if (row === 0) {
+          html += '</tr>';
+          html += '</thead>';
+          html += '<tbody>';
+        } else {
+          html += '</tr>';
+        };
+    };
+    html += '</tbody>';
+    html += '</table>';
     $('#table').append(html);
-});
+ };
